e72c03f3cb69e6f881d995a66555dbee0cca4469
Josephwallen <70412480+Josephwallen@users.noreply.github.com>
2020-11-28 17:53:24 +0000

    making readable

--- Call.py
+++ Call.py
@@ -7,17 +7,18 @@ from twilio.rest import Client
 
 
 def Call(phonenumber,Filename):
-    #create XML file
-    
+    #Create XML file with audiofile to allow for the call to access 
     Response = ET.Element("Response")
     ET.SubElement(Response, "Play").text = 'http://nevercallagain.frost.cx/audio/'+Filename+'.mp3'
 
     tree = ET.ElementTree(Response)
     tree.write(Filename + ".xml")
     
+    #getting twilio authorisation
     account_sid = os.environ['TWILIO_ACCOUNT_SID']
     auth_token = os.environ['TWILIO_AUTH_TOKEN']
     client = Client(account_sid, auth_token)
+    #make the call using phonenumber and audio
 
     call = client.calls.create(
         url='http://nevercallagain.frost.cx/twiml/'+Filename+'.Xml',
