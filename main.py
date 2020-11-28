9f049a2779220375bbb67495fb08f8b856a1b6f5
James Frost <git@frost.cx>
2020-11-28 15:41:22 +0000

    add print

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ def incoming_sms():
     """Send a dynamic reply to an incoming text message"""
     # Get the message the user sent our Twilio number
     body = request.values.get('Body', None)
+    print(body)
     # Start our TwiML response
     resp = MessagingResponse()
     # Determine the right reply for this message
